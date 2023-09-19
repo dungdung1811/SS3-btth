71fc58ec011892e35546e1407b86b772a6d3a4c0
dungdung1811 <nguyendung181192@gmail.com>
2023-09-19 21:22:00 +0700

    btth-ss3

---
+++ src/ra/bt3/Main.java
@@ -0,0 +1,26 @@
+package ra.bt3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập một năm: ");
+        int year = sc.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.printf("%d Là năm nhuận", year);
+                } else {
+                    System.out.printf("%d Không phải là năm nhuận", year);
+                }
+            } else {
+                System.out.printf("%d Là năm nhuận", year);
+            }
+        } else {
+            System.out.printf("%d Không phải là năm nhuận", year);
+        }
+
+    }
+}
